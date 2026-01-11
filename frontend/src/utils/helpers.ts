ac67a0159630703983d2e6ea3e6203f5c5349164
Smridhi Gupta <91938698+smridhiwho@users.noreply.github.com>
2026-01-11 12:45:44 +0530

    deployment fixes

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -43,7 +43,8 @@ export function debounce<T extends (...args: unknown[]) => unknown>(
   func: T,
   wait: number
 ): (...args: Parameters<T>) => void {
-  let timeout: NodeJS.Timeout | null = null
+  let timeout: number | null = null
+
 
   return function executedFunction(...args: Parameters<T>) {
     const later = () => {
